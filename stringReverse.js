13975662fe31f483046f399373ff02eab1ba8a13
Nikhil Butani <nikhilbutani17898@gmail.com>
2022-05-25 00:00:29 +0530

    String Reverse Problem Solution

---
+++ stringReverse.js
@@ -0,0 +1,7 @@
+function builtInReverse(str) {
+    return str.split('').reverse().join('');
+}
+
+console.log(builtInReverse("30 days of coding challenge"));
+console.log(builtInReverse("This is Javascript"));
+console.log(builtInReverse("Nikhil Butani"));
